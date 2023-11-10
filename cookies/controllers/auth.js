638d3a46ac1b1b8f5ffdcf2ed84133f476154d6e
andrelmm91 <andre.lmm91@gmail.com>
2023-11-10 14:01:17 +0100

    session data on mongodb and fix

--- cookies/controllers/auth.js
+++ cookies/controllers/auth.js
@@ -1,26 +1,29 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  res.render("auth/login", {
-    path: "/login",
-    pageTitle: "Login",
-    isAuthenticated: false,
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
-    .then((user) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      res.redirect("/");
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err) => {
-    err ? console.log(err) : null;
-    res.redirect("/");
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
   });
 };
