03ba76f1c733c4109341579a580bc91be635d4c4
andrelmm91 <andre.lmm91@gmail.com>
2023-11-10 14:51:23 +0100

    bcrypt password and session update

---
+++ authentication/app.js
@@ -0,0 +1,78 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const mongoDBSession = require("connect-mongodb-session")(session);
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+const { mongodbConnection } = require("./mongodb/mongodbConnection");
+
+const app = express();
+const store = new mongoDBSession({
+  uri: mongodbConnection,
+  collection: "sessions",
+});
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+const helmet = require("helmet");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(helmet());
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use(
+  session((req, res, next) => {
+    req.session.user ? null : next();
+    User.findById(req.session.user._id)
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((err) => console.log(err));
+  })
+);
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(mongodbConnection + "?retryWrites=true", { useNewUrlParser: true })
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Max",
+          email: "max@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+      return user;
+    });
+    console.log("connected to mongoDB");
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
