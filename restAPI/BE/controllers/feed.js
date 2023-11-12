a7cf7d4d01d8803d1700a4ec3bdd22fc14c715ac
andrelmm91 <andre.lmm91@gmail.com>
2023-11-12 14:54:07 +0100

    docker and validation

--- restAPI/BE/controllers/feed.js
+++ restAPI/BE/controllers/feed.js
@@ -1,14 +1,14 @@
+const { validationResult } = require("express-validator"); //validator
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
         _id: "1",
-        title: "first",
+        title: "firstaaaoi",
         content: "first",
         imagesUrl: "images/dog.jpeg",
-        creator: {
-          name: "And",
-        },
+        creator: { name: "And" },
         createdAt: new Date(),
       },
     ],
@@ -16,11 +16,21 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPosts = (req, res, next) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(422).json({ errors: error.array() });
+  }
   const title = req.body.title;
   const content = req.body.content;
   //create db
   res.status(201).json({
     message: "ok!",
-    post: { id: new Date().toISOString(), title: title, content: content },
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "And" },
+      createdAt: new Date(),
+    },
   });
 };
--- restAPI/BE/routes/feed.js
+++ restAPI/BE/routes/feed.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const feedController = require("../controllers/feed");
+const { body } = require("express-validator"); //validator
 
 // GET / feed/posts
 router.get("/posts", feedController.getPosts);
 // POST / feed/posts
-router.post("/posts", feedController.createPosts);
+router.post(
+  "/posts",
+  [
+    body("title")
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage("Title must be at least 6 characters long"),
+    body("content")
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage("Content must be at least 6 characters long"),
+  ],
+  feedController.createPosts
+);
 
 module.exports = router;
