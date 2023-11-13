9419a4178bcb75e4d36529d50002339cd36aa4d9
andrelmm91 <andre.lmm91@gmail.com>
2023-11-13 15:59:17 +0100

    middleware to all routes

--- restAPI/BE/routes/feed.js
+++ restAPI/BE/routes/feed.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const feedController = require("../controllers/feed");
 const { body } = require("express-validator"); //validator
 
+const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
+
 // GET / feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 // POST / feed/posts
 router.post(
   "/posts",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -16,10 +19,11 @@ router.post(
   feedController.createPosts
 );
 
-router.get("/post/:postId", feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 
 router.patch(
   "/posts/:postId",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -27,6 +31,6 @@ router.patch(
   feedController.updatePost
 );
 
-router.delete("/post/:postId", feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
